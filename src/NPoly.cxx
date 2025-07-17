802c017920d3f4d8e27f3088c0245e65c77648c1
sethcarl2000 <sethhall@jlab.org>
2025-07-17 09:46:00 -0400

    First commit - added NPoly class

---
+++ src/NPoly.cxx
@@ -0,0 +1,286 @@
+//*-- Author :    Seth Hall   09-Jul-24
+
+//////////////////////////////////////////////////////////////////////////
+//     
+// NPoly
+// This is the backbone of the 'reverse optics' model.
+// effectivley, all it is is a polynomial, with a appendable list of elements,
+// defined on some R^n space.
+// In the case of the reverse-optics, it will be defined on an R^4 space,
+//  + using 3 polynomials in conjunction lets you make a f:R^4->R^3 map.
+//  
+//     
+//////////////////////////////////////////////////////////////////////////
+
+#include "NPoly.h"
+#include <cmath>
+#include <iostream>
+
+using namespace ROOT::VecOps; 
+using vecd = RVec<double>; 
+
+using namespace std;
+
+//_____________________________________________________________________________
+NPoly::NPoly(const int nDoF, const int order) :
+  fnDoF(nDoF)
+{  
+  if (order>0) {
+    fOrder = order;
+    AutoConstructPoly(order,nDoF);
+  }
+}
+//_____________________________________________________________________________
+NPoly::~NPoly() {/*destructor*/};
+//_____________________________________________________________________________
+vecd NPoly::Eval_noCoeff(const vecd &X) const
+{
+  //check to make sure that the input vector is the right size
+  if ((int)X.size() != Get_nDoF()) {
+    Error("NPoly::Eval_noCoeff",
+	  "Size of input vector (%i) does not match poly nDoF (%i)",
+	  (int)X.size(), Get_nDoF());
+    return {};
+  }
+
+  vecd vals; vals.reserve(Get_nElems());
+  
+  for (const NPolyElem &elem : fElems) {
+
+    double elem_val=1.;
+
+    for (int d=0; d<Get_nDoF(); d++) {
+      if (elem.powers[d] > 0) elem_val *= pow( X[d], elem.powers[d] );
+    }
+    //check to see if this is the one element for which all pows. are zero
+    vals.push_back( elem_val );
+  }
+
+  return vals;
+}
+//_____________________________________________________________________________
+double NPoly::Eval(const vecd &coeff, const vecd &X) const
+{
+  if ((int)coeff.size() != Get_nElems()) { 
+    Error("Eval(vecd,vecd)",
+	  "Wrong number of coeffs. given: expected %i, recieved %i.",
+	  Get_nElems(),
+	  (int)coeff.size() );
+    return -1e30;
+  }
+        
+  if ((int)X.size() != Get_nDoF()) { 
+    Error("Eval(vecd,vecd)", "Size of input vector (%i) does not match poly nDoF (%i)",
+	  (int)X.size(), Get_nDoF());
+    return -1e30;
+  }
+      
+  //now, actually evaluate
+  double val=0.; 
+
+  //for each element, raise each val in X to the right power, the multiply
+  // by the coressponding coeff.
+  //for each element, raise each val in X to the right power, the multiply
+  // by the coressponding coeff.
+  int i=0; 
+  for (const auto &elem : fElems) {
+    
+    double elem_val=1.;
+    
+    for (int d=0; d<Get_nDoF(); d++) {
+      if (elem.powers[d]>0) elem_val *= pow( X[d], elem.powers[d] );
+    }
+    //check to see if this is the one element for which all pows. are zero
+    val += elem_val * coeff[i++]; 
+	
+  }//for (const auto &elem : fElems)
+
+  /*for (unsigned int i=0; i<Get_nElems(); i++) { const auto& elem = fElems[i]; 
+    
+    double elem_val=1.;
+    
+    for (int d=0; d<Get_nDoF(); d++) {
+      if (elem.powers[d]>0) elem_val *= pow( X[d], elem.powers[d] );
+    }
+    //check to see if this is the one element for which all pows. are zero
+    val += elem_val * coeff[i]; 
+	
+  }//for (i=0; i<Get_nElems(); i++) */
+      
+  return val;
+}
+//_____________________________________________________________________________
+double NPoly::Eval(const vecd &X) const 
+{
+  //same as above, but use the coefficients 'hard-coded' to each element
+  
+  if ((int)X.size() != Get_nDoF()) { 
+    Error("Eval(vecd)", "Size of input vector (%i) does not match poly nDoF (%i)",
+	  (int)X.size(), (int)Get_nDoF());
+    return -1e30;
+  }
+      
+  //now, actually evaluate
+  double val=0.; 
+
+  //for each element, raise each val in X to the right power, the multiply
+  // by the coressponding coeff.
+  for (const auto &elem : fElems) {
+    
+    double elem_val=1.;
+    
+    for (int d=0; d<Get_nDoF(); d++) {
+      if (elem.powers[d]>0) elem_val *= pow( X[d], elem.powers[d] );
+    }
+    //check to see if this is the one element for which all pows. are zero
+    val += elem_val * elem.coeff; 
+	
+  }//for (const auto &elem : fElems)
+      
+  return val;
+}
+//_____________________________________________________________________________
+vecd NPoly::Gradient(const vecd &coeff, const vecd &X) const
+{
+  if (coeff.size() != Get_nElems()) { 
+    Error("NPoly::Eval()",
+	  "Wrong number of coeffs. given; expected %i, recieved %i.",
+	  Get_nElems(), (int)coeff.size() );
+    return {};
+  }
+  
+  if ((int)X.size() != fnDoF) { 
+    Error("NPoly::Eval_noCoeff",
+	  "Size of input vector (%i) does not match poly nDoF (%i)",
+	  (int)X.size(), fnDoF);
+    return {};
+  }
+
+  vecd grad(fnDoF,0.); 
+  
+  //for each element, raise each val in X to the right power, the multiply
+  // by the coressponding coeff. 
+  for (unsigned int i=0; i<Get_nElems(); i++) { auto elem = fElems[i];
+    
+    vecd grad_elem(fnDoF,1.); 
+
+    for (int g=0; g<fnDoF; g++) { 
+
+      int pow_deriv = elem.powers[g];
+      if (pow_deriv<1) {
+        grad_elem[g] = 0.; continue; 
+      } else { 
+        grad_elem[g] *= ((double)pow_deriv)*pow(X[g], pow_deriv-1);
+      } 
+
+      //now, deal with the elems we are NOT taking the deriv of 
+      for (int d=0; d<fnDoF; d++) { if (d==g) continue; 
+        if (elem.powers.at(d)>0) { 
+          grad_elem[g] *= pow( X.at(d), elem.powers.at(d) );
+        }
+      }
+    }
+
+    
+    grad += coeff.at(i) * grad_elem; 
+  }
+  
+  //for (i=0; i<Get_nElems(); i++) 
+  return grad;
+}
+//_____________________________________________________________________________
+RVec<int> NPoly::Get_elemPowers(unsigned int i) const
+{
+  if (i >= Get_nElems()) {
+    Warning("NPoly::Get_elemPowers",
+	    "Invalid element-index requested; %i, max is %i",i,Get_nElems()-1);
+    return {};
+  }
+  return fElems.at(i).powers;
+}
+//_____________________________________________________________________________
+double NPoly::Get_elemCoeff(unsigned int i) const
+{
+  if (i >= Get_nElems()) {
+    Warning("Get_elemCoeff",
+	    "Invalid element-index requested; %i, max is %i",i,Get_nElems()-1);
+    return {};
+  }
+  return fElems.at(i).coeff;
+}
+//_____________________________________________________________________________
+NPoly::NPolyElem* NPoly::Get_elem(unsigned int i) 
+{
+  if (i >= Get_nElems()) {
+    Warning("Get_elemCoeff",
+	    "Invalid element-index requested; %i, max is %i",i,Get_nElems()-1);
+    return nullptr;
+  }
+  return &fElems[i];
+}
+//_____________________________________________________________________________
+void NPoly::Add_element(const RVec<int> &pows, double coefficient)
+{
+  if (pows.size()!=fnDoF) {
+    Error("Add_element", "Num. of powers (%i) incorrect size for this Poly's DoF (%i)",
+	  (int)pows.size(), (int)fnDoF);
+    return;
+  }
+  fElems.push_back({.powers=pows, .coeff=coefficient}); 
+}
+//_____________________________________________________________________________
+void NPoly::AutoConstructPoly(const int max_power, const int nDoF)
+{
+  RVec<int> powers(max_power,0); 
+  
+  while (1) {
+    //create a new poly element with our current 'powers'
+    //auto elem = NPolyElem;
+    RVec<int> pows; 
+    
+    for (int x=0; x<nDoF; x++) {
+      int myPow=0;
+      for (int xPow : powers) if (xPow==x) myPow++;
+      
+      pows.push_back(myPow);
+    }
+    
+    /*cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl; 
+      for (int p=0; p<max_power; p++) { 
+      for (int d=0; d<nDoF+1; d++) cout << (powers.at(p)==d?1:0) << " ";
+      cout << endl; 
+      }*/ 
+
+    /*cout << "elem : "; 
+      for (int d=0; d<nDoF; d++) cout << elem->powers.at(d) << ","; 
+      cout << endl; */ 
+
+    fElems.push_back({.powers=pows, .coeff=1.});
+    
+    //now, increase to the next set of powers
+    for (int it1=powers.size()-1; it1>=0; it1--) { 
+      //go back up and reset higher powers
+            
+      if (powers.at(it1)<nDoF) {
+        powers.at(it1) += 1; 
+        for (unsigned int it2=it1+1; it2<powers.size(); it2++) {
+          powers.at(it2) = powers.at(it1);
+        }
+        break; 
+      }
+      
+      if (it1==0) {
+        //cout << "polynomial size = " << elems.size() << endl; 
+        return; 
+      }
+            
+    }//for (int it1=powers.size()-1 ... [power-escalating group]        
+  }//while (1)
+}
+//_____________________________________________________________________________
+//_____________________________________________________________________________
+//_____________________________________________________________________________
+//_____________________________________________________________________________
+
+ClassImp(NPoly)
+
---
+++ src/NPoly.h
@@ -0,0 +1,65 @@
+#ifndef NPoly_h_
+#define NPoly_h_
+
+//////////////////////////////////////////////////////////////////////////
+//
+// TOpticsModel
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "TROOT.h"
+#include "TObject.h"
+#include <ROOT/RVec.hxx> 
+
+class NPolyElem; 
+
+class NPoly : public TObject {
+  
+ public:
+
+  NPoly() {}; 
+  NPoly(int nDoF, int order=0); 
+  ~NPoly();
+  
+  //return the value of each element evaluated against the input vector
+  ROOT::RVec<double> Eval_noCoeff(const ROOT::RVec<double> &X) const;
+  
+  double Eval(const ROOT::RVec<double> &coeff, const ROOT::RVec<double> &X) const; 
+  double Eval(const ROOT::RVec<double> &X) const;
+  
+  ROOT::RVec<double> Gradient(const ROOT::RVec<double> &coeff, const ROOT::RVec<double> &X) const; 
+  
+  //handle elements + get information
+  void Add_element(const ROOT::RVec<int> &pows, double coefficient=1.);
+  
+  //get the number of elements currently in the polynomial
+  inline unsigned int Get_nElems() const { return fElems.size(); }
+  
+  //get this polynomial's input DoF
+  inline unsigned int Get_nDoF() const { return fnDoF; }
+
+  struct NPolyElem {
+    ROOT::RVec<int>  powers; //powers for each (x,y,th,ph) in target-coordinates  
+    double coeff;            //in most cases, coefficients are not needed per poly.
+  };
+
+  //Get a pointer to an individual element
+  NPolyElem*          Get_elem      (unsigned int i); 
+  //Get the RVec<int> of the powers of each input var for this element
+  ROOT::RVec<int>     Get_elemPowers(unsigned int i) const;
+  //get the coefficient of this element
+  double              Get_elemCoeff (unsigned int i) const;
+  
+private:
+  
+  int fOrder,fnDoF;
+  
+  ROOT::RVec<NPolyElem> fElems;
+  
+  //construct all possible polynomial elemts for which the sum of the exponents for all DoF is <= max_power. 
+  void AutoConstructPoly(const int max_power, const int nDoF); 
+  
+  ClassDef(NPoly,1);   
+};
+
+#endif
