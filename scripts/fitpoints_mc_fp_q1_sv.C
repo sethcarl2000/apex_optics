31730d11cade47fd0e0968f9020b07c7d5d72292
sethcarl2000 <sethhall@jlab.org>
2025-07-22 13:14:43 -0400

    Refactor fitpoints_mc_fp_q1_sv.C to use ApexOptics::Create_NPoly_fit method for NPoly coefficient generation

--- scripts/fitpoints_mc_fp_q1_sv.C
+++ scripts/fitpoints_mc_fp_q1_sv.C
@@ -10,104 +10,6 @@
 
 using namespace std; 
 
-//this is a helper function which will take the names of of the 'X_elems_*' branches as input, along with the target 
-//output branches, and return a std::map<string, NPoly*>, where the key of each element is the name of the polynomial. 
-//______________________________________________________________________________________________________________________________
-map<string,NPoly*> find_bestfit_poly_coeffs( ROOT::RDF::RNode df, 
-                                             NPoly *poly_template, 
-                                             const char* X_elems_name, 
-                                             vector<string> outputs )
-{
-    const char* const here = "find_bestfit_poly_coeffs";
-
-    const int nDoF    = poly_template->Get_nDoF(); 
-    const int n_elems = poly_template->Get_nElems();
-
-    const int n_outputs = outputs.size(); 
-
-    ROOT::RDF::RResultPtr<double> A_elems[n_elems][n_elems]; 
-
-    for (int i=0; i<n_elems; i++) {
-        for (int j=0; j<n_elems; j++) {
-            A_elems[i][j] = df
-                .Define("val", [i,j](const ROOT::RVec<double> &X){ return X[i]*X[j]; }, {X_elems_name}).Sum("val"); 
-        }
-    }
-
-    //these are the 'outputs'; what the polynomial maps onto. 
-    //the 'outside' vector will have .size()=num_of_output_branches (output_branches.size()), and the 'inside' vector  
-    // will have .size()=num_of_input_branches
-    const int n_out_branches = outputs.size(); 
-    ROOT::RDF::RResultPtr<double> B_ptr[n_elems][n_outputs];  
-    
-    int i_out=0; 
-    for (const string& str : outputs) {
-
-        //'book' the calculations for each element
-        for (int i=0; i<n_elems; i++) {
-            //this RResultPtr<double> will be the sum of the branch 'str' (see the construction of the output_branches vector above)
-            //we call '.data()' method to get a 'char*' that the string is wrapping
-            B_ptr[i][i_out] = df
-                .Define("val", [i](const ROOT::RVec<double> &X, double y){ return X[i] * y; }, {X_elems_name, str.data()}).Sum("val"); 
-        }
-        i_out++; 
-    }
-
-    //this matrix will be used to find the best coefficients for each element 
-    RMatrix A(n_elems, n_elems); 
-
-    vector<double> b_vec[n_outputs]; 
-
-    cout << "--filling matrix..." << flush; 
-    //now, fill the matrix, and the 'b' values
-    for (int i=0; i<n_elems; i++) {
-
-        for (int obr=0; obr<n_outputs; obr++) {
-            b_vec[obr].push_back( *(B_ptr[i][obr]) ); 
-        }
-
-        for (int j=0; j<n_elems; j++) {
-            A.get(i,j) = *(A_elems[i][j]);
-        }  
-    }
-    cout << "done." << endl; 
-
-    //now, we can actually solve the linear equation for the tensor coefficients. 
-    //we put this in 'std::map' form, so that we can access any of the 'coefficient vectors' indexed by the name of the input branch. 
-    map<string, vector<double>> poly_coeffs; 
-
-    cout << "--Solving linear system(s)..." << flush; 
-    //use the RMatrix::Solve() method to solve the system of lin. equations corresponding to each 'output'. Store the answer in our map. 
-    int obr=0; 
-    for (const string& out_branch : outputs) {
-        poly_coeffs[out_branch] = A.Solve( b_vec[obr++] ); 
-    }
-    cout << "done." << endl; 
-
-    //now, create the std::map which will store our output polynomials, indexed by their names. 
-    map<string, NPoly*> poly_map;
-    
-    for (auto it = poly_coeffs.begin(); it != poly_coeffs.end(); it++) {
-
-        //get polynomial name and vector of coefficients
-        string         poly_name = it->first; 
-        vector<double> coeff_vec = it->second; 
-
-        //create a new NPoly, and add it to our output map  
-        NPoly *poly = new NPoly(nDoF); 
-        poly_map[poly_name] = poly; 
-            
-        for (int i=0; i<poly_template->Get_nElems(); i++) { 
-            
-            NPoly::NPolyElem *elem = poly_template->Get_elem(i); 
-            poly->Add_element( elem->powers, coeff_vec.at(i) );
-        }
-    }
-
-    return poly_map; 
-}
-//______________________________________________________________________________________________________________________________
-
 //if you hand this helper function an 'fstream' object, and a sdt::map<string, NPoly*>, it will output all the elements of each
 // polynomial into the dbfile. 
 //______________________________________________________________________________________________________________________________
@@ -182,7 +84,6 @@ int fitpoints_mc_fp_q1_sv(  bool is_RHRS=false,
         Error(here, "libApexOptics could not be loaded."); 
         return 1; 
     }
-
     
     //check if the infile could be opened
     if (!infile || infile->IsZombie()) {
@@ -208,27 +109,9 @@ int fitpoints_mc_fp_q1_sv(  bool is_RHRS=false,
     ROOT::RDataFrame df(tree_name, path_infile); 
 
     
-    const int nDoF_fpq1 = 4; //the 4 DoF are: x_fp, y_fp, dxdz_fp, dydz_fp
-
-    const int nDoF_q1sv = 5; //the 5 DoF are: x_q1, y_q1, dxdz_q1, dydz_q1, dpp_q1
-
     const int hrs_momentum = 1104.0; 
 
-    auto poly_fpq1 = new NPoly(nDoF_fpq1, poly_fpq1_order); 
-    auto poly_q1sv = new NPoly(nDoF_q1sv, poly_q1sv_order); 
-
     auto df_output = df
-
-        .Define("X_elems_fpq1", [poly_fpq1](double x, double y, double dxdz, double dydz)
-        {
-            return poly_fpq1->Eval_noCoeff({
-                x,              //x_fp 
-                y,              //y_fp
-                dxdz - x/6.,    //dxdz_fp
-                dydz            //dydz_fp
-            }); 
-
-        }, {"x_fp", "y_fp", "dxdz_fp", "dydz_fp"})
 
         .Redefine("dxdz_fp", [](double x_tra, double dxdz_tra){ return dxdz_tra - x_tra/6.;}, {"x_fp", "dxdz_fp"})
 
@@ -328,10 +211,8 @@ int fitpoints_mc_fp_q1_sv(  bool is_RHRS=false,
     create_dbfile_from_polymap(is_RHRS, path_outfile, polymap_q1sv); 
 
     //delete our template polynomial, and all model polynomials
-    delete poly_fpq1; 
     for (auto it = polymap_fpq1.begin(); it != polymap_fpq1.end(); it++ ) delete it->second;
 
-    delete poly_q1sv; 
     for (auto it = polymap_q1sv.begin(); it != polymap_q1sv.end(); it++ ) delete it->second;
 
 
