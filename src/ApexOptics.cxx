1dc519e3f6ebe7e3ba1adc7485303a19f2e28796
sethcarl2000 <sethhall@jlab.org>
2025-07-22 11:20:31 -0400

    Add checking for existence of columns in 'Create_NPoly_fit'

--- src/ApexOptics.cxx
+++ src/ApexOptics.cxx
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstdio>
+#include <cmath>
 #include <ROOT/RVec.hxx> 
 #include <ROOT/RResultPtr.hxx>
 #include "TMatrixD.h"
@@ -17,10 +18,45 @@ unique_ptr<NPoly> ApexOptics::Create_NPoly_fit( ROOT::RDF::RNode df,
                                                 const vector<string> &inputs, 
                                                 const char* output )
 {
+    const char* const here = "ApexOptics::Create_NPoly_fit"; 
+    
     const int nDoF = inputs.size();  
 
-    if (nDoF < 1 ) return unique_ptr<NPoly>(new NPoly(0)); 
+    if (nDoF < 1 ) {
+        fprintf(stderr, "Error in <%s>: No inputs given.\n", here); 
+        return unique_ptr<NPoly>(nullptr); 
+    }
 
+    //check for the existence of all required columns    
+    vector<string> column_names = df.GetColumnNames(); 
+    vector<string> missing_columns{}; 
+    
+    vector<string> all_columns_needed = inputs; inputs.push_back(string(output)); 
+
+    //loop through all branches needed, check if any are missing.
+    for ( const string& column_needed : all_columns_needed ) { 
+        
+        bool is_found(false);    
+        
+        for ( const string& column_found : column_names ) if (column_found == column_needed) is_found = true; 
+        
+        if (!is_found) missing_columns.push_back(column_needed); 
+    }
+
+    //some missing columns were found. 
+    if (missing_columns.size()>0) {
+        fprintf(stderr, "Error in <%s>: Missing columns: ", here); 
+        for (const string& col : missing_columns) {
+            fprintf(stderr, "'%s' ", col.data()); 
+        }
+        cerr << "\n"; 
+        return unique_ptr<NPoly>(nullptr); 
+    }
+
+    return unique_ptr<NPoly>(nullptr); 
+    
+
+    //now that we know all the necessary columns exist, we can proceed: 
     //first, define the vector of inputs. 
     // since this function can take a variable number of inputs, we need to 'tack' each input onto a single vector. 
     vector<ROOT::RDF::RNode> df_vec{df.Define("inputs", [](double _inp){return RVec<double>{};}, {inputs[0].data()})}; 
@@ -40,20 +76,22 @@ unique_ptr<NPoly> ApexOptics::Create_NPoly_fit( ROOT::RDF::RNode df,
         df_vec.push_back(new_df); 
     }
 
+    //The polynomial we output will follow the same template as this one:
+    auto poly_template = unique_ptr<NPoly>(new NPoly(nDoF, poly_order)); 
+
+    const int n_elems = poly_template->Get_nElems(); 
+
    
     //now that we have put all the inputs into a vector, we can continue. 
     auto df_output = df_vec.at(df_vec.size()-1)
     
-        .Define("X_elems", [poly](const RVec<double> &inputs) 
+        .Define("X_elems", [poly_template](const RVec<double> &inputs) 
         {
-            return poly->Eval_noCoeff(inputs); 
+            return poly_template->Eval_noCoeff(inputs); 
         } {"inputs"}); 
 
 
-    auto poly = unique_ptr<NPoly>(new NPoly(nDoF, poly_order)); 
-
-    const int n_elems = poly->Get_nElems(); 
-
+    
     ROOT::RDF::RResultPtr<double> A_ptr[n_elems][n_elems]; 
 
     for (int i=0; i<n_elems; i++) {
@@ -77,39 +115,47 @@ unique_ptr<NPoly> ApexOptics::Create_NPoly_fit( ROOT::RDF::RNode df,
     }
     
     //this matrix will be used to find the best coefficients for each element 
-    RMatrix A(n_elems, n_elems); 
+    TMatrixD A(n_elems, n_elems); 
+    TVectorD B(n_elems); 
 
-    vector<double> b_vec[n_outputs]; 
-
+    //now, fill the matrix, and the 'b' values. 
+    // When we request access to the RResultPtr objects created above (which is what the B_ptr and A_ptr arrays are), 
+    // this is the moment that the RDataFrame actually loops through all events in the dataframe.  
     cout << "--filling matrix..." << flush; 
-    //now, fill the matrix, and the 'b' values
     for (int i=0; i<n_elems; i++) {
-
-        for (int obr=0; obr<n_outputs; obr++) {
-            b_vec[obr].push_back( *(B_ptr[i][obr]) ); 
-        }
-
+        
+        B(i) =  *(B_ptr[i]); 
+        
         for (int j=0; j<n_elems; j++) {
-            A.get(i,j) = *(A_elems[i][j]);
+            A(i,j) = *(A_ptr[i][j]);
         }  
     }
     cout << "done." << endl; 
 
-    //now, we can actually solve the linear equation for the tensor coefficients. 
-    //we put this in 'std::map' form, so that we can access any of the 'coefficient vectors' indexed by the name of the input branch. 
-    map<string, vector<double>> poly_coeffs; 
-
-    cout << "--Solving linear system(s)..." << flush; 
-    //use the RMatrix::Solve() method to solve the system of lin. equations corresponding to each 'output'. Store the answer in our map. 
-    int obr=0; 
-    for (const string& out_branch : outputs) {
-        poly_coeffs[out_branch] = A.Solve( b_vec[obr++] ); 
+    //check to see if TMatrixD considers this matrix to be singular 
+    if (fabs(A.Determinant()) < 1e-10) {
+        fprintf(stderr, "Error in <%s>: A-matrix is singular (|det| < 1e-16)", here); 
+        return unique_ptr<NPoly>(nullptr); 
     }
+
+    //now, we can actually solve the linear equation for the tensor coefficients. 
+    cout << "--Solving linear system(s)..." << flush; 
+    
+    //Solve the linear system of equations to get our best-fit coefficients
+    auto coeffs = A.Invert() * B; 
+
+    auto poly = unique_ptr<NPoly>(new NPoly(nDoF)); 
+
+    for (int i=0; i<n_elems; i++) { 
+        
+        //we're gonna make a copy of each element in the 'template' polynomial
+        NPoly::NPolyElem *elem = poly_template->Get_elem(i); 
+
+        //now, we just add our coefficient we just computed to it
+        poly->Add_element( elem->powers, coeffs(i) ); 
+    }
+
     cout << "done." << endl; 
-
-
-
-
 
     return poly; 
 }
