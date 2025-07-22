560520d95e7d53f128f4f982a3c869df7a98cfdf
sethcarl2000 <sethhall@jlab.org>
2025-07-22 19:56:20 -0400

    Change to use of 'Parse_NPoly_from_file' method from ApexOptics namespace

--- scripts/newton_iteration_test.C
+++ scripts/newton_iteration_test.C
@@ -2,92 +2,6 @@
 
 using namespace std; 
 
-//this will be a temporary funct., that I may absorb into NPoly.cxx. 
-int parse_poly_from_file(const char* path_dbfile, const char* poly_name, NPoly *poly) 
-{
-    //uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuer89999999999999999999999999999999999uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
-    // -bear 
-
-    //parse each line separately. in each line, each 'token' is separated by whitespace. 
-    //each line must have the following format: 
-    //
-    //      dxdz_sv   0   0   2   0   -3.044778720e-01
-    //
-    //The first token is the name of the polynomial to which this element belongs.
-    // the next 4 tokens are the power to which this polynomial must be raised. 
-    // the last token is the coefficient of this element.
-    
-    //The file must be headed by the line: 
-    //
-    //      poly-DoF 4
-    
-    const char* const here = "parse_poly_from_file"; 
-
-    //open the db file
-    ifstream dbfile(path_dbfile); 
-    
-    if (!dbfile.is_open()) {
-        Error(here, "unable to open db file '%s'", path_dbfile); 
-        return -1; 
-    }
-
-    //now, read the file
-    string line; 
-    
-    //get the DoF of this poly
-    getline(dbfile, line); 
-    istringstream iss_init(line);
-    
-    string token; 
-
-    iss_init >> token; 
-    if (token != "poly-DoF") {
-        Error(here, "Missing 'poly-DoF [n]' header at top of dbfile '%s'", path_dbfile); 
-        return -1; 
-    }
-
-    int poly_DoF; 
-    iss_init >> poly_DoF; 
-
-    if (poly_DoF != poly->Get_nDoF()) {
-        //Error(here, "Poly DoF in db file (%i) does not match DoF of passed Poly (%i)", 
-        //    poly_DoF, poly->Get_nDoF()); 
-        //return -1;
-        //this polynomial cannot belong to this file, it has the wrong DoF! 
-        return 0;  
-    }
-
-    int start_nElems = poly->Get_nElems(); 
-
-    //now, we can ready the rest of the file. 
-    while (getline(dbfile, line)) {
-
-        //parse the string into token (delimited by whitespace!)
-        istringstream iss(line); 
-
-        string elem_name; iss >> elem_name; 
-
-        //this line is not the poly you're looking for
-        if (elem_name != poly_name) continue; 
-        
-        //now, we can read the powers / coefficient
-        RVec<int> powers(poly_DoF, 0); 
-        double coeff; 
-
-        //read the powers
-        for (int &pow : powers) iss >> pow;
-
-        //read the coefficient
-        iss >> coeff; 
-
-        poly->Add_element(powers, coeff); 
-    }
-    
-    dbfile.close(); 
-
-    return poly->Get_nElems() - start_nElems; //noop
-}
-
 
 //creates db '.dat' files for polynomials which are meant to map from focal-plane coordinates to sieve coordinates. 
 int fitpoints_mc_sv_fp(  bool is_RHRS=false,
@@ -147,12 +61,12 @@ int fitpoints_mc_sv_fp(  bool is_RHRS=false,
 
         NPoly poly(DoF_sv); 
 
-        parse_poly_from_file(path_dbfile, str.data(), &poly); 
+        ApexOptics::Parse_NPoly_from_file(path_dbfile, str.data(), &poly); 
 
         poly_vec.push_back(poly); 
     }
 
     //now that we have created the polys, we can create the NPolyModel object
     NPolyModel pmod(poly_vec); 
-    
+
 }
