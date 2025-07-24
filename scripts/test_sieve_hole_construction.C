3b800fab016c2fa51d858bb4eb2c209938689e0a
sethcarl2000 <sethhall@jlab.org>
2025-07-24 19:53:44 -0400

    Make test_sieve_hole_construction.C macro functional

--- scripts/test_sieve_hole_construction.C
+++ scripts/test_sieve_hole_construction.C
@@ -51,7 +51,7 @@ struct SieveHole {
 }; 
 
 struct Track_t {
-    double x,y,dxdz,dydz; 
+    TVector3 pos,mom; 
 };
 
 int test_sieve_hole_construction(const bool is_RHRS=true, const int vwire_num=2, const int n_events=1e5)
@@ -63,8 +63,12 @@ int test_sieve_hole_construction(const bool is_RHRS=true, const int vwire_num=2,
     auto twister   = mt19937(rd());  
     
     auto real_dist = std::uniform_real_distribution<double>(0., 1.); 
-    auto Get_rnd = [&real_dist,&twister](){ return real_dist(twister); }; 
+    auto Get_rnd_range = [&real_dist,&twister](double min, double max)
+    { 
+        return min + (max-min)*real_dist(twister); 
+    }; 
 
+    vector<SieveHole> big_holes; 
 
     //create RHRS sieve-holes
     vector<SieveHole> sieve_holes; 
@@ -79,6 +83,9 @@ int test_sieve_hole_construction(const bool is_RHRS=true, const int vwire_num=2,
     const double x0 = -dx * 8.; 
     //the first column is 7-spaces +y from the center
     const double y0 =  dy * 7.; 
+
+    //sieve thickness
+    const double sieve_thickness = 12.7; 
 
     //two possible hole radii
     const double holeR_small = 0.6985; 
@@ -135,6 +142,8 @@ int test_sieve_hole_construction(const bool is_RHRS=true, const int vwire_num=2,
             }
 
             sieve_holes.push_back(new_hole); 
+            
+            if (bigHole) { big_holes.push_back(new_hole); }
 
         }//for (int col=0; col<nCols; col++) 
     }//for (int row=0; row<nRows; row++) 
@@ -152,39 +161,151 @@ int test_sieve_hole_construction(const bool is_RHRS=true, const int vwire_num=2,
         return -1; 
     }
     
-    TVector3 vwire_pos = vwire_positions.at(vwire_num-1); 
+    TVector3 vertex = vwire_positions.at(vwire_num-1); 
     
     //change the wire pos to sieve coordinates
-    vwire_pos.RotateY( ApexTargetGeometry::Get_sieve_angle(is_RHRS) ); 
-    vwire_pos.RotateZ( TMath::Pi()/2. ); 
+    vertex.RotateY( ApexTargetGeometry::Get_sieve_angle(is_RHRS) ); 
+    vertex.RotateZ( TMath::Pi()/2. ); 
+
+    //add the offsets of the sieve-plane center
+    vertex += -1.* ApexTargetGeometry::Get_sieve_pos(is_RHRS); 
+ 
     
-    //create a function which will return a random sieve-hole
+    //create a function which will return a random sieve-hole. This is designed to give weight to holes proportional to their front-size.
+    double total_area =0.; 
+    vector<double> area_weights{};
+    for (const auto& hole : sieve_holes) {
+
+        double hole_opening_area = hole.radius_front * hole.radius_front;  
+        area_weights.push_back( total_area );
+        total_area += hole_opening_area;  
+    }
+    
+
     const vector<SieveHole> *holes = &sieve_holes; 
 
-    auto int_dist = uniform_int_distribution<int>(0., sieve_holes.size()-1); 
-    auto Random_hole = [int_dist, twister, holes]() 
+    //so... the large holes are **approximately** 4 times the area of the other holes. so I will give each 'big' hole 3+ 'ballots' in the raffle of which holes to populate. 
+    //the gist of this subroutine is that we want to 'throw' tracks at each hole, in an amount which is proportional to the opening-face
+    //of the hole. to do this efficiently, we choose all holes randomly, by having an 'index' generated over a random distribution. 
+    // however, we must account for the fact that there are two 'big' holes. To account for this, we give each of the big holes an 
+    // extra 'weight', which amounts to an extra chance at selection, each time a random hole is picked. 
+    auto int_dist = uniform_int_distribution<int>(-6, sieve_holes.size()-1); 
+    /*auto Random_hole = [&Get_rnd_range, &area_weights, total_area, holes]() 
     {
-        return holes->at( int_dist(twister) ); 
+        double sample = Get_rnd_range(0., total_area); 
+        for (int i=holes->size()-1; i>=0; i--) {
+            if (area_weights.at(i) < sample ) return holes->at(i); 
+        }
+        Error("Random_hole", "Got to end of sieve-hole list! should not have happened...\n"); 
+        return SieveHole{}; 
+    }; */
+
+    //area ratio of 'big hole' to 'small holes'. 
+    //so... the large holes are **approximately** 4 times the area of the other holes. so I will give each 'big' hole 3+ 'ballots' in the raffle of which holes to populate. 
+    //the gist of this subroutine is that we want to 'throw' tracks at each hole, in an amount which is proportional to the opening-face
+    //of the hole. to do this efficiently, we choose all holes randomly, by having an 'index' generated over a random distribution. 
+    // however, we must account for the fact that there are two 'big' holes. To account for this, we give each of the big holes an 
+    // extra 'weight', which amounts to an extra chance at selection, each time a random hole is picked. 
+    double big_hole_extra_area = (holeR_big*holeR_big)/(holeR_small*holeR_small) - 1.; 
+
+    auto Random_hole = [holes, &big_holes, &Get_rnd_range, big_hole_extra_area]() 
+    {
+        double index = Get_rnd_range( -2.*big_hole_extra_area, (double)holes->size()-1 ); 
+        
+        if (index < -big_hole_extra_area) return big_holes.at(0); 
+        if (index < 0. )                  return big_holes.at(1); 
+
+        return holes->at( (int)index ); 
     }; 
+    
 
 
 
 
-    printf("Generating %i tracks...", n_events); cout << flush;  
+    ROOT::EnableImplicitMT(); 
+    printf("Multi-threadding enabled. Thread pool size: %i\n", ROOT::GetThreadPoolSize() ); 
+
+    printf("Generating %.2e tracks...", (double)n_events); cout << endl;
+
+
 
     ROOT::RDataFrame df(n_events);
 
     auto df_output = df 
 
-        .Define("x_fp",     [Random_hole]()
-        {
+        .Define("Xsv",     [Random_hole, Get_rnd_range, vertex, sieve_thickness]()
+        {   
+            TVector3 pos_sieve, momentum; 
+
             //keep looping until we find a 'good' target trajectory
+            bool found=false; 
+
+            while (!found) {
+                
+                SieveHole hole = Random_hole(); 
+                
+                int tries=0; 
+
+                while (1) { //choose a random spot to 'aim' at
+                    
+                    pos_sieve = TVector3(
+                        Get_rnd_range( -hole.radius_front, hole.radius_front ),
+                        Get_rnd_range( -hole.radius_front, hole.radius_front ),
+                        0. 
+                    ); 
+
+                    //check if this 'spot' is within the radius of the hole 
+                    if ( pos_sieve.Mag() < hole.radius_front ) break; 
+                }
+
+                double x_exit = pos_sieve.x();
+                double y_exit = pos_sieve.y(); 
+
+                pos_sieve[0] += hole.x; 
+                pos_sieve[1] += hole.y; 
+
+                momentum = (pos_sieve - vertex).Unit(); 
+                
+                //now, see if this track can 'thread the needle' from the entrance hole, out of the exit hole. 
+                x_exit += (momentum.x()/momentum.z()) * sieve_thickness; 
+                y_exit += (momentum.y()/momentum.z()) * sieve_thickness; 
+                
+                //If the hole hits the wall of the sieve hole before it exists, then throw this one out. 
+                if ( sqrt(x_exit*x_exit + y_exit*y_exit) < hole.radius_back ) break; 
+
+                //while (tries++ < 25)
             
-            const SieveHole = Random_hole(); 
+            }//while (!found)
+                
+            
 
+            Track_t track_sv{
+                .pos = pos_sieve, 
+                .mom = momentum
+            }; 
 
+            return track_sv; 
+        }) 
 
-        })
+        .Define("x_sv", [](Track_t &trk){ return trk.pos.x(); }, {"Xsv"})
+        .Define("y_sv", [](Track_t &trk){ return trk.pos.y(); }, {"Xsv"});
+
+    
+    string h_title = (is_RHRS ? "Sieve Hole drawing - RHRS" : "Sieve Hole drawing - LHRS"); 
+    
+    char buff[50]; sprintf(buff, " (V%i)", vwire_num);
+    h_title += string(buff); 
+
+    h_title += ";x_sv;y_sv"; 
+    
+
+    auto hist = df_output.Histo2D({"h", h_title.data(), 300, -50, 50, 300, -50, 50}, "x_sv", "y_sv"); 
+    
+    gStyle->SetPalette(kSunset); 
+    
+    hist->DrawCopy("col2"); 
+    
+    cout << "done." << endl; 
 
     //uncomment this to draw all sieve holes. 
     /*new TCanvas; 
