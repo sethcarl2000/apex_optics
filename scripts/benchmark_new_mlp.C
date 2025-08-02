9e65c029d4be9115511a9b68e981aea146b59771
sethcarl2000 <sethhall@jlab.org>
2025-08-02 13:07:53 -0400

    Updated 'benchmark_new_mlp.C' to include benchmarking of NPoly

--- scripts/benchmark_new_mlp.C
+++ scripts/benchmark_new_mlp.C
@@ -7,20 +7,24 @@ using namespace ROOT::VecOps;
 int benchmark_new_mlp()
 {
 
-    MultiLayerPerceptron mlp({5,5,5,5,4}); 
+    RVec<int> mlp_structure{5,8,8,8,4}; 
+
+    MultiLayerPerceptron mlp(mlp_structure); 
 
     //initialize network with random, gaussian weights
     for (int l=0; l<mlp.Get_n_layers()-1; l++) {
         for (double& weight : mlp.Get_layer(l)) weight = gRandom->Gaus(); 
     }
+    cout << "MLP structure (inputs => hidden layers => outputs): " << mlp_structure << endl; 
 
     const int n_trials = 1e7; 
 
     RVec<double> X(mlp.Get_DoF_in(), 0.); 
     RVec<double> Z(mlp.Get_DoF_out(), 0.); 
+    double z; 
 
     NPoly poly(5); 
-    NPoly pol_template(5,6); 
+    NPoly pol_template(5,5); 
     
     
     for (int i=0; i<pol_template.Get_nElems(); i++) poly.Add_element( pol_template.Get_elemPowers(i), gRandom->Gaus() ); 
@@ -31,15 +35,18 @@ int benchmark_new_mlp()
     for (int i=0; i<n_trials * (mlp.Get_DoF_in()); i++) { double x = gRandom->Gaus(); }
     double time_gaus = timer_gaus.RealTime(); 
 
+    cout << "Starting evaluation..." << flush; 
     TStopwatch timer; 
     for (int i=0; i<n_trials; i++) {
 
         //initialize random input vector
         for (double& x : X) x = gRandom->Gaus(); 
 
-        double z = poly.Eval(X); 
+        z = poly.Eval(X); 
+        //Z = mlp.Eval(X); 
     }
     double time_eval = timer.RealTime() - time_gaus; 
+    cout << "done." << endl; 
 
     printf(" %f seconds elapsed to process %i events (%f us/event)\n", time_eval, n_trials, 1e6 * time_eval/((double)n_trials)); 
 
