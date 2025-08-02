3614efd4b2032f27c805061a3be27ca76f4f400d
sethcarl2000 <sethhall@jlab.org>
2025-08-02 13:29:43 -0400

    Change 'benchmark_new_mlp.C' to compare MLP time to an NPolyArray of comparable size

--- scripts/benchmark_new_mlp.C
+++ scripts/benchmark_new_mlp.C
@@ -4,35 +4,51 @@
 using namespace std; 
 using namespace ROOT::VecOps; 
 
+//true when benchmarking poly, false when benchmarking MLP
+#define BENCHMARK_POLY false
+
 int benchmark_new_mlp()
 {
-
     RVec<int> mlp_structure{5,8,8,8,4}; 
 
     MultiLayerPerceptron mlp(mlp_structure); 
 
+#if BENCHMARK_POLY
+
+    const int poly_order = 3; 
+    
+    NPoly pol_template(mlp.Get_DoF_in(), poly_order); 
+
+    //fill the poly-array with polynomials (of the proper order), with random-gauss coefficients
+    vector<NPoly> poly_vec; 
+    for (int i=0; i<mlp.Get_DoF_out(); i++) { 
+        NPoly poly(mlp.Get_DoF_in()); 
+        for (int e=0; e<pol_template.Get_nElems(); e++) poly.Add_element( pol_template.Get_elemPowers(e), gRandom->Gaus() ); 
+        poly_vec.push_back(poly); 
+    }
+
+    NPolyArray parr(poly_vec); 
+
+    printf("Number of poly-array elements: %i\n", pol_template.Get_nElems() * mlp.Get_DoF_out()); 
+    printf("poly-array DoF in/out: %i/%i, poly-array order: %i\n", parr.Get_DoF_in(), parr.Get_DoF_out(), poly_order); 
+#else 
     //initialize network with random, gaussian weights
     for (int l=0; l<mlp.Get_n_layers()-1; l++) {
         for (double& weight : mlp.Get_layer(l)) weight = gRandom->Gaus(); 
     }
     cout << "MLP structure (inputs => hidden layers => outputs): " << mlp_structure << endl; 
+#endif 
 
     const int n_trials = 1e7; 
 
-    RVec<double> X(mlp.Get_DoF_in(), 0.); 
-    RVec<double> Z(mlp.Get_DoF_out(), 0.); 
+    RVec<double> X(mlp.Get_DoF_in(),    0.); 
+    RVec<double> Z(mlp.Get_DoF_out(),   0.); 
     double z; 
 
-    NPoly poly(5); 
-    NPoly pol_template(5,5); 
     
-    
-    for (int i=0; i<pol_template.Get_nElems(); i++) poly.Add_element( pol_template.Get_elemPowers(i), gRandom->Gaus() ); 
-    cout << "Number of polynomial elements: " << poly.Get_nElems() << endl; 
-
     //measure this, so we know how must of the elapsed time measured below is due to 'gRandom->Gaus()' 
     TStopwatch timer_gaus; 
-    for (int i=0; i<n_trials * (mlp.Get_DoF_in()); i++) { double x = gRandom->Gaus(); }
+    for (int i=0; i<n_trials * (mlp.Get_DoF_in()); i++) { z = gRandom->Gaus(); }
     double time_gaus = timer_gaus.RealTime(); 
 
     cout << "Starting evaluation..." << flush; 
@@ -41,9 +57,11 @@ int benchmark_new_mlp()
 
         //initialize random input vector
         for (double& x : X) x = gRandom->Gaus(); 
-
-        z = poly.Eval(X); 
-        //Z = mlp.Eval(X); 
+#if BENCHMARK_POLY 
+        Z = parr.Eval(X); 
+#else 
+        Z = mlp.Eval(X);
+#endif  
     }
     double time_eval = timer.RealTime() - time_gaus; 
     cout << "done." << endl; 
