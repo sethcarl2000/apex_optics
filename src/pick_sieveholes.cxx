7b6a259ca79ff0cd36c6a402d77b7831c8046eae
sethcarl2000 <sethhall@jlab.org>
2025-08-13 10:22:49 -0400

    Add first version of pick_sieveholes app

---
+++ src/pick_sieveholes.cxx
@@ -0,0 +1,14 @@
+#include "TROOT.h"
+#include "TApplication.h"
+#include "NPoly.h"
+#include <iostream> 
+
+using namespace std; 
+
+int main(int argc, char* argv[]) 
+{
+    cout << "here..." << endl; 
+    
+    return 0; 
+}
+
