49be43bac1f15446e28911e29f950c3c99d0ad7d
sethcarl2000 <sethhall@jlab.org>
2025-09-11 14:15:37 -0400

    Update 'make_fitdata_with_MC_model' to distinguish between 'fwd' and 'rev' coordinate reconstructions

--- scripts/make_fitdata_with_MC_model.C
+++ scripts/make_fitdata_with_MC_model.C
@@ -5,6 +5,9 @@
 #include <ROOT/RVec.hxx>
 #include "include/RDFNodeAccumulator.h"
 #include <iostream> 
+#include <TSystem.h> 
+#include <TFile.h> 
+#include <TParameter.h> 
 
 using namespace std; 
 using namespace ROOT::VecOps; 
@@ -41,19 +44,74 @@ ROOT::RDF::RNode add_branch_from_Trajectory_t(ROOT::RDF::RNode df, const char* b
 int make_fitdata_with_MC_model(const char* path_infile, const char* path_outfile) 
 {
     const char* const here = "make_fitdata_with_MC_model"; 
+    
+    const char* tree_name = "tracks_fp"; 
+
+    //check if we can load the apex optics lib
+    if (gSystem->Load("libApexOptics") < 0) {
+        Error(here, "libApexOptics could not be loaded."); 
+        return 1; 
+    }
+    
+    //check if the infile could be opened
+    auto infile = new TFile(path_infile, "READ"); 
+    if (!infile || infile->IsZombie()) {
+        Error(here, "root file '%s' could not be opened.", path_infile); 
+        return 1; 
+    }
+
+    //check if we can find the proper TTree
+    TTree* tree = (TTree*)infile->Get(tree_name); 
+    if (!tree) {
+        Error(here, "could not find TTree '%s'", tree_name); 
+        return 1; 
+    }
+
+    //check if we can find the 'is_RHRS' parameter. Fatal error if not! 
+    TParameter<bool>* param_is_RHRS = (TParameter<bool>*)infile->Get("is_RHRS"); 
+    if (!param_is_RHRS) {
+        Error(here, "Could not find TParameter<bool> 'is_RHRS' in file '%s'.", path_infile); 
+        return 1; 
+    }
+    const bool is_RHRS = param_is_RHRS->GetVal(); 
+       
+    delete tree; 
+    infile->Close(); 
+    delete infile;
 
     //this polynomial will only be used to compute dp/p, as a funciton of sieve coordinates. 
-    const char* path_NPolyArray_dpp   = "data/csv/poly_prod_fp_sv_L_6ord.dat"; 
-    const char* path_NPolyArray_sv_q1 = "data/csv/poly_prod_sv_q1_L_6ord.dat"; 
-    const char* path_NPolyArray_q1_fp = "data/csv/poly_prod_q1_fp_L_6ord.dat"; 
-    
+    const char* path_NPolyArray_dpp   = "data/csv/poly_prod_fp_sv_L_5ord.dat";  // dp/p <======= fp
+
+    const char* path_NPolyArray_sv_q1 = "data/csv/poly_prod_sv_q1_L_5ord.dat";  //   sv => q1 
+    const char* path_NPolyArray_q1_fp = "data/csv/poly_prod_q1_fp_L_5ord.dat";  //         q1 => fp 
+    const char* path_NPolyArray_sv_fp = "data/csv/poly_prod_sv_fp_L_5ord.dat";  //   sv =======> fp 
+
+    const char* path_NPolyArray_fp_sv = "data/csv/poly_prod_fp_sv_L_5ord.dat";  //   sv <======= fp 
+    const char* path_NPolyArray_fp_q1 = "data/csv/poly_prod_fp_q1_L_5ord.dat";  //         q1 <= fp 
+    const char* path_NPolyArray_q1_sv = "data/csv/poly_prod_q1_sv_L_5ord.dat";  //   sv <= q1 
+
+
     //try to parse the polynomials from files. catch any exceptions, if they occur. 
-    NPolyArray parr_fp_dpp, parr_sv_q1, parr_q1_fp; 
+    NPolyArray parr_fp_dpp; 
+    
+    NPolyArray parr_sv_q1, parr_q1_fp, parr_sv_fp; 
+    NPolyArray parr_fp_sv, parr_fp_q1, parr_q1_sv;  
+
+    const vector<string> branches_sv{"x_sv","y_sv","dxdz_sv","dydz_sv","dpp_sv"}; 
+    const vector<string> branches_q1{"x_q1","y_q1","dxdz_q1","dydz_q1","dpp_q1"}; 
+    const vector<string> branches_fp{"x_fp","y_fp","dxdz_fp","dydz_fp"}; 
+
     
     try {
         parr_fp_dpp = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_dpp,   {"dpp_sv"}, 4); 
-        parr_sv_q1  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_sv_q1, {"x_q1", "y_q1", "dxdz_q1", "dydz_q1", "dpp_q1"}, 5); 
-        parr_q1_fp  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_q1_fp, {"x_fp", "y_fp", "dxdz_fp", "dydz_fp"}, 5); 
+
+        parr_sv_q1  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_sv_q1, branches_q1, 5); 
+        parr_q1_fp  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_q1_fp, branches_fp, 5);
+        parr_sv_fp  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_sv_fp, branches_fp, 5);
+
+        parr_fp_sv  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_fp_sv, branches_sv, 4); 
+        parr_fp_q1  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_fp_q1, branches_q1, 4);
+        parr_q1_sv  = ApexOptics::Parse_NPolyArray_from_file(path_NPolyArray_q1_sv, branches_sv, 5);
     
     } catch (const std::exception& e) {
         
@@ -63,7 +121,6 @@ int make_fitdata_with_MC_model(const char* path_infile, const char* path_outfile
 
     
     //now, try to construct the RDataFrame. 
-
     ROOT::RDataFrame *df = nullptr; 
 
     try {        
@@ -95,42 +152,69 @@ int make_fitdata_with_MC_model(const char* path_infile, const char* path_outfile
 
     }, {"x_sv", "y_sv", "dxdz_sv", "dydz_sv", "Xfp"}); 
 
-    rna.Define("Xq1_reco", [&parr_sv_q1](Trajectory_t Xsv)
-    {   
-        RVec<double>&& Xsv_rvec = ApexOptics::Trajectory_t_to_RVec(Xsv); 
+    //propagate the 'real' Xsv to XQ1 and Xfp
+    rna.Define("Xq1_fwd", [&parr_sv_q1](Trajectory_t Xsv)
+    {
+        return ApexOptics::RVec_to_Trajectory_t(
+            parr_sv_q1.Eval(ApexOptics::Trajectory_t_to_RVec(Xsv))
+        ); 
+    }, {"Xsv"});
 
-        Trajectory_t&& Xq1 = ApexOptics::RVec_to_Trajectory_t(parr_sv_q1.Eval(Xsv_rvec)); 
+    rna.Define("Xfp_fwd", [&parr_q1_fp](Trajectory_t Xq1)
+    {
+        return ApexOptics::RVec_to_Trajectory_t(
+            parr_q1_fp.Eval(ApexOptics::Trajectory_t_to_RVec(Xq1))
+        ); 
+    }, {"Xq1_fwd"});
 
-        return Xq1; 
+    //propagate the 'real' Xfp to XQ1 and Xsv
+    rna.Define("Xq1_rev", [&parr_fp_q1](Trajectory_t Xfp)
+    {
+        return ApexOptics::RVec_to_Trajectory_t(
+            parr_fp_q1.Eval(ApexOptics::Trajectory_t_to_RVec(Xfp))
+        ); 
+    }, {"Xfp"});
 
-    }, {"Xsv"}); 
+    rna.Define("Xsv_rev", [&parr_q1_sv](Trajectory_t Xq1)
+    {
+        return ApexOptics::RVec_to_Trajectory_t(
+            parr_q1_sv.Eval(ApexOptics::Trajectory_t_to_RVec(Xq1))
+        ); 
+    }, {"Xq1_rev"});
 
-    rna.Define("Xfp_reco", [&parr_q1_fp](Trajectory_t Xq1)
-    {   
-        RVec<double>&& Xq1_rvec = ApexOptics::Trajectory_t_to_RVec(Xq1); 
+    //add the forward reconstructions 
+    rna = add_branch_from_Trajectory_t(rna.Get(), "Xq1_fwd", {
+        {"fwd_x_q1",    &Trajectory_t::x}, 
+        {"fwd_y_q1",    &Trajectory_t::y},
+        {"fwd_dxdz_q1", &Trajectory_t::dxdz},
+        {"fwd_dydz_q1", &Trajectory_t::dydz},
+        {"fwd_dpp_q1",  &Trajectory_t::dpp} 
+    });
 
-        Trajectory_t&& Xfp = ApexOptics::RVec_to_Trajectory_t(parr_q1_fp.Eval(Xq1_rvec)); 
-
-        return Xfp; 
-
-    }, {"Xq1_reco"}); 
-
-    rna = add_branch_from_Trajectory_t(rna.Get(), "Xq1_reco", {
-        {"reco_x_q1",    &Trajectory_t::x}, 
-        {"reco_y_q1",    &Trajectory_t::y},
-        {"reco_dxdz_q1", &Trajectory_t::dxdz},
-        {"reco_dydz_q1", &Trajectory_t::dydz},
-        {"reco_dpp_q1",  &Trajectory_t::dpp} 
+    rna = add_branch_from_Trajectory_t(rna.Get(), "Xfp_fwd", {
+        {"fwd_x_fp",    &Trajectory_t::x}, 
+        {"fwd_y_fp",    &Trajectory_t::y},
+        {"fwd_dxdz_fp", &Trajectory_t::dxdz},
+        {"fwd_dydz_fp", &Trajectory_t::dydz}
     }); 
 
-    rna = add_branch_from_Trajectory_t(rna.Get(), "Xfp_reco", {
-        {"reco_x_fp",    &Trajectory_t::x}, 
-        {"reco_y_fp",    &Trajectory_t::y},
-        {"reco_dxdz_fp", &Trajectory_t::dxdz},
-        {"reco_dydz_fp", &Trajectory_t::dydz}
-    }); 
 
+    //add the backward reconstructions
+    rna = add_branch_from_Trajectory_t(rna.Get(), "Xq1_rev", {
+        {"rev_x_q1",    &Trajectory_t::x}, 
+        {"rev_y_q1",    &Trajectory_t::y},
+        {"rev_dxdz_q1", &Trajectory_t::dxdz},
+        {"rev_dydz_q1", &Trajectory_t::dydz},
+        {"rev_dpp_q1",  &Trajectory_t::dpp} 
+    });
 
+    rna = add_branch_from_Trajectory_t(rna.Get(), "Xsv_rev", {
+        {"rev_x_sv",    &Trajectory_t::x}, 
+        {"rev_y_sv",    &Trajectory_t::y},
+        {"rev_dxdz_sv", &Trajectory_t::dxdz},
+        {"rev_dydz_sv", &Trajectory_t::dydz},
+        {"rev_dpp_sv",  &Trajectory_t::dpp} 
+    });
 
     cout << "Making the snapshot..." << flush; 
 
@@ -138,27 +222,42 @@ int make_fitdata_with_MC_model(const char* path_infile, const char* path_outfile
     try {
         
         rna.Get().Snapshot("tracks_fp", path_outfile, {
+            //real data branches
             "x_sv",
             "y_sv",
             "dxdz_sv",
             "dydz_sv",
             "dpp_sv",
-
-            "reco_x_q1",
-            "reco_y_q1",
-            "reco_dxdz_q1",
-            "reco_dydz_q1",
-            "reco_dpp_q1",
-
-            "reco_x_fp",
-            "reco_y_fp",
-            "reco_dxdz_fp",
-            "reco_dydz_fp",
-
+    
             "x_fp", 
             "y_fp",
             "dxdz_fp",
-            "dydz_fp"
+            "dydz_fp",
+
+            //forward reconstructions
+            "fwd_x_q1",
+            "fwd_y_q1",
+            "fwd_dxdz_q1",
+            "fwd_dydz_q1",
+            "fwd_dpp_q1",
+
+            "fwd_x_fp",
+            "fwd_y_fp",
+            "fwd_dxdz_fp",
+            "fwd_dydz_fp",
+
+            //reverse reconstructions
+            "rev_x_q1",
+            "rev_y_q1",
+            "rev_dxdz_q1",
+            "rev_dydz_q1",
+            "rev_dpp_q1",
+
+            "rev_x_sv",
+            "rev_y_sv",
+            "rev_dxdz_sv",
+            "rev_dydz_sv",
+            "rev_dpp_sv"
         }); 
 
     } catch (const std::exception& e) {
@@ -168,6 +267,15 @@ int make_fitdata_with_MC_model(const char* path_infile, const char* path_outfile
         return -1; 
     }
     cout << "done.\nOuptut file created: '" << path_outfile << "'" << endl; 
+
+    //add the 'is_RHRS' parameter to the file
+    infile = new TFile(path_outfile, "UPDATE"); 
+    
+    param_is_RHRS = new TParameter<bool>("is_RHRS", is_RHRS); 
+    param_is_RHRS->Write(); 
+    
+    infile->Close(); 
+    delete infile; 
 
     return 0; 
 }
