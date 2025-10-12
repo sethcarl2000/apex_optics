538733df2e41c33333d70480933c3ea42fbe6aa1
sethcarl2000 <sethhall@jlab.org>
2025-10-11 21:52:48 -0400

    Add working version of 2d-gaussin fit

--- scripts/gaussfit_2d.C
+++ scripts/gaussfit_2d.C
@@ -8,9 +8,11 @@
 #include <TRandom3.h>  
 #include <TGraph.h> 
 #include <cmath> 
+#include <vector> 
 #include <iostream> 
 #include <Math/Factory.h> 
 #include <Math/Minimizer.h>
+#include <Math/Functor.h>
 
 using namespace std; 
 
@@ -21,7 +23,7 @@ double fcn_2d_gauss(const double *X, const double* par)
     const double& x0   = par[1]; 
     const double& y0   = par[2];
     const double a_xx  = fabs(par[3]); 
-    const double a_xy  = fabs(par[4]);
+    const double a_xy  = par[4];
     const double a_yy  = fabs(par[5]);
     
     const double x = X[0] - x0;
@@ -29,6 +31,124 @@ double fcn_2d_gauss(const double *X, const double* par)
 
     return amplitude * exp( -0.5*((x*x*a_xx) + (x*y*a_xy) + (y*y*a_yy)) );
 }
+
+//template<typename T> struct MinMaxVal_t { T min, max; }; 
+
+vector<double> Fit_TF2_to_TH2D(TH2D* hist, TF2* fcn_inp, const std::vector<std::pair<std::string,double>>& varlist)
+{   
+    //first, collect all the bin contents we need
+    auto x_ax = hist->GetXaxis();
+    auto y_ax = hist->GetYaxis();
+
+    //make a copy of the input fcn we can mess with
+    auto fcn = (TF2*)fcn_inp->Clone(); 
+
+    const int n_fcn_params = fcn->GetNpar(); 
+
+    double fit_domain_x0, fit_domain_x1; 
+    double fit_domain_y0, fit_domain_y1; 
+
+    fcn->GetRange( 
+        fit_domain_x0, fit_domain_x1, 
+        fit_domain_y0, fit_domain_y1 
+    ); 
+
+    fit_domain_x0 = min<double>( fit_domain_x0, x_ax->GetXmin() ); 
+    fit_domain_y0 = min<double>( fit_domain_y0, y_ax->GetXmin() ); 
+    
+    fit_domain_x1 = max<double>( fit_domain_x1, x_ax->GetXmax() ); 
+    fit_domain_y1 = max<double>( fit_domain_y1, y_ax->GetXmax() ); 
+    
+    const int 
+        bin_x_min{ x_ax->FindBin(fit_domain_x0) },
+        bin_x_max{ x_ax->FindBin(fit_domain_x1) }; 
+
+    const int 
+        bin_y_min{ y_ax->FindBin(fit_domain_y0) },
+        bin_y_max{ y_ax->FindBin(fit_domain_y1) }; 
+    
+    //get the bin contents from the histogram
+    const int n_bins_x = bin_x_max - bin_x_min; 
+    const int n_bins_y = bin_y_max - bin_y_min; 
+
+    struct BinContent_t { double x, y, n; }; 
+    vector<BinContent_t> bin_contents; bin_contents.reserve(n_bins_x * n_bins_y); 
+
+    for (int ix=bin_x_min; ix<=bin_x_max; ix++) {
+        for (int iy=bin_y_min; iy<=bin_y_max; iy++) {
+
+            bin_contents.push_back({
+                x_ax->GetBinCenter(ix),
+                y_ax->GetBinCenter(iy), 
+                hist->GetBinContent(ix, iy) 
+            }); 
+        }
+    }
+
+    ROOT::Math::Minimizer *minimizer = ROOT::Math::Factory::CreateMinimizer("Minuit2", "Migrad"); 
+    
+    if (!minimizer) return {}; 
+    
+    minimizer->SetMaxFunctionCalls(1e7); 
+    minimizer->SetMaxIterations(1e6); 
+    minimizer->SetTolerance(1e-3);
+    minimizer->SetPrintLevel(0);    
+
+    //simple function to compute log liklihood
+    //____________________________________________________________________________________________________
+    auto LogLiklihood = [&bin_contents, &fcn](const double* par) {
+
+        fcn->SetParameters(par);
+
+        double log_liklihood = 0.;
+
+        //array of x-vals which we will pass as the fcn argument
+        double X[] = {0., 0.}; 
+
+        //loop thru all bins
+        for (const auto& bin : bin_contents) {
+            
+            X[0] = bin.x; 
+            X[1] = bin.y; 
+
+            double fcn_val  = fcn_2d_gauss(X, par);// //fcn->Eval(bin.x, bin.y);  //fcn_2d_gauss(X, par);
+
+            //histogram bin counting starts from '1' and goes to 'ninbs', inclusive. 
+
+            //use stirling's approx 
+            double log_nfact = bin.n > 0. ? log_nfact = bin.n * log(bin.n) - bin.n : 0.; 
+
+            //this is just the log of the poisson dist, taken with n=hist_val, and <n>=fcn_val
+            log_liklihood += (bin.n * log(fcn_val)) - fcn_val - log_nfact; 
+        }
+        return -1.*log_liklihood; 
+    };
+    //___________________________________________________________________________________________________________
+
+    auto f_minimizer = ROOT::Math::Functor(LogLiklihood, n_fcn_params);
+    
+    minimizer->SetFunction(f_minimizer);
+
+    //set the list of variables
+    int i_var=0; 
+    for (const auto& var : varlist) {
+        minimizer->SetVariable(i_var++, var.first.c_str(), var.second, 1e-4);
+    }
+
+    auto status = minimizer->Minimize();
+
+    cout << "minimizer status: " << std::boolalpha << status << endl; 
+
+    const double* par_result = minimizer->X(); 
+
+    if (!par_result) return {}; 
+
+    vector<double> vals{}; 
+    for (int i=0; i<n_fcn_params; i++) vals.push_back(par_result[i]); 
+    
+    return vals; 
+} 
+
 
 int gaussfit_2d()
 {
@@ -42,9 +162,18 @@ int gaussfit_2d()
         6                   // number of parameters
     ); 
 
+    
+    auto hist = new TH2D("h", "test", 60, -5, 5, 60, -5, 5); 
+    TRandom3 rand; 
+    for (int i=0; i<1e5; i++) {
+        double x = rand.Gaus(); 
+        double y = rand.Gaus() - 0.4*x; 
+        hist->Fill( x, y ); 
+    }
+
     //look at fcn above for detailed def.
     double params[] = {
-        1.,     //amplitude
+        hist->GetMaximum(),     //amplitude
         0.,     //x-mean
         0.,     //y-mean
         1.,     // a_xx
@@ -52,75 +181,21 @@ int gaussfit_2d()
         1.,     // a_yy
     }; 
     tf2->SetParameters(params); 
-
-    auto hist = new TH2D("h", "test", 60, -5, 5, 60, -5, 5); 
-    TRandom3 rand; 
-    for (int i=0; i<1e5; i++) {
-        hist->Fill( rand.Gaus(), rand.Gaus() ); 
-    }
-
-    //compute the log-liklihood of a hist with a given TF2
-    auto LogLiklihood = [](TH2D* hist, TF2* fcn) 
-    {
-        double log_liklihood = 0.; 
-
-        const int nbins_x = hist->GetXaxis()->GetNbins(); 
-        const int nbins_y = hist->GetYaxis()->GetNbins(); 
-
-        double* bin_x_ptr = new double[nbins_x]; hist->GetXaxis()->GetCenter(bin_x_ptr); 
-        double* bin_y_ptr = new double[nbins_y]; hist->GetYaxis()->GetCenter(bin_y_ptr); 
-
-        for (int ix=0; ix<nbins_x; ix++) {
-
-            const double x = bin_x_ptr[ix];
-
-            for (int iy=0; iy<nbins_y; iy++) {
-
-                const double y = bin_y_ptr[iy];
-
-                double fcn_val  = fcn->Eval(x, y); 
-
-                //histogram bin counting starts from '1' and goes to 'ninbs', inclusive. 
-                double hist_val = hist->GetBinContent(ix+1, iy+1); 
-
-                //use stirling's approx 
-                double log_nfact = hist_val > 0. ? log_nfact = hist_val * log(hist_val) - hist_val : 0.; 
-
-                //this is just the log of the poisson dist, taken with n=hist_val, and <n>=fcn_val
-                log_liklihood += (hist_val * log(fcn_val)) - fcn_val - log_nfact; 
-            }
-        }
-        return log_liklihood; 
-    };
+    
+    auto fit_params = Fit_TF2_to_TH2D(hist, tf2, {
+        {"amplitude",   hist->GetMaximum()},
+        {"x0",          1.},
+        {"y0",          0.},
+        {"a_xx",        1.},
+        {"a_xy",        0.},
+        {"a_yy",        1.}
+    });
+    
+    tf2->SetParameters(fit_params.data()); 
 
     hist->Draw("col2"); 
-
-    vector<double> pts_x, pts_LL; 
-
-    double x0 = -2.; 
-    for (int i=0; i<201; i++) {
-
-        tf2->SetParameter(1, x0);
-
-        pts_x.push_back(x0); 
-
-        double LL = LogLiklihood(hist, tf2);
-
-        pts_LL.push_back(LL);
-
-        x0 += 0.02; 
-
-        printf("x %+.3f, LL: %.3e", x0, LL); cout << endl;      
-    }
-
-    tf2->SetParameter(0, hist->GetMaximum()); 
+    //tf2->SetParameter(0, hist->GetMaximum()); 
     tf2->Draw("SAME"); 
 
-    new TCanvas; 
-    auto graph = new TGraph(pts_x.size(), pts_x.data(), pts_LL.data()); 
-
-    graph->SetTitle("Log Liklihood vs x_{0};x_{0};L.L.");
-    graph->Draw(); 
-    
     return 0; 
 }
