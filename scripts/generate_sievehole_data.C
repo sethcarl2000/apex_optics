e50e7415615a624f3b5137ca55a8830209ba2306
sethcarl2000 <sethhall@jlab.org>
2025-10-13 19:42:55 -0400

    Add version of 'generate_sievehole_data.C' which uses 2d-gaussain fit

---
+++ scripts/generate_sievehole_data.C
@@ -0,0 +1,308 @@
+//apex src headers
+#include <SieveHole.h> 
+#include <ApexOptics.h> 
+//local 'scripts/include' folder headers
+#include "include/ChainedOpticsModel.h"
+#include "include/Fit_gauss_to_TH2D.h"
+#include "include/RDFNodeAccumulator.h"
+#include "include/Add_TParameter_to_TFile.h"
+#include "include/Fit_gauss_to_TH2D.h"
+//ROOT headers
+#include <ROOT/RDataFrame.hxx>
+#include <TCanvas.h> 
+#include <TStyle.h> 
+#include <TBox.h> 
+//std lib headers
+#include <vector>
+#include <string> 
+#include <optional> 
+#include <stdexcept>
+#include <cmath>  
+
+using namespace std; 
+using ApexOptics::Trajectory_t; 
+using ApexOptics::OpticsTarget_t; 
+
+const vector<string> branches_sv{"x_sv","y_sv","dxdz_sv","dydz_sv","dpp_sv"};
+const vector<string> branches_q1{"x_q1","y_q1","dxdz_q1","dydz_q1","dpp_q1"};
+const vector<string> branches_fp{"x_fp","y_fp","dxdz_fp","dydz_fp"};
+
+const vector<string> branches_fwd_q1{"fwd_x_q1","fwd_y_q1","fwd_dxdz_q1","fwd_dydz_q1","fwd_dpp_q1"};
+const vector<string> branches_fwd_fp{"fwd_x_fp","fwd_y_fp","fwd_dxdz_fp","fwd_dydz_fp"};
+
+const vector<string> branches_rev_sv{"fwd_x_sv","fwd_y_sv","fwd_dxdz_sv","fwd_dydz_sv","fwd_dpp_sv"};
+const vector<string> branches_rev_q1{"fwd_x_q1","fwd_y_q1","fwd_dxdz_q1","fwd_dydz_q1","fwd_dpp_q1"};
+
+//Given a TH2*, find the maximum bin-value in the subrange given by [x0,x1] & [y0,y1]. 
+double GetMaximum_in_subrange(TH2* hist, double x0,double y0, double x1,double y1)
+{   
+    //check if the hist is null. 
+    if (!hist) {
+        throw std::invalid_argument("in <Max_bin_in_subrange>: null TH2* object was passed."); 
+        return 0.; 
+    }
+
+    auto x_ax = hist->GetXaxis(); 
+    auto y_ax = hist->GetYaxis(); 
+
+    double max_content=-1.e30; 
+
+    for (int bx=x_ax->FindBin(x0); bx<=x_ax->FindBin(x1); bx++) {
+        for (int by=y_ax->FindBin(y0); by<=y_ax->FindBin(y1); by++) {
+
+            max_content = max<double>( max_content, hist->GetBinContent(bx, by) ); 
+        }
+    }
+    return max_content; 
+}
+
+
+//_______________________________________________________________________________________________________________________________________________
+//if you want to use the 'fp-sv' polynomial models, then have path_dbfile_2="". otherwise, the program will assume that the *first* dbfile
+// provided (path_dbfile_1) is the q1=>sv polynomials, and the *second* dbfile provided (path_dbfile_2) are the fp=>sv polynomials. 
+int generate_sievehole_data(const char* path_infile     = "data/replay/real_L_V2-dp.root",
+                            const char* target_name     = "V2",
+                            const char* tree_name       = "tracks_fp" ) 
+{
+    const char* const here = "cleanup_optics_replay";
+    
+    ROOT::EnableImplicitMT(); 
+    ROOT::RDataFrame df(tree_name, path_infile); 
+
+    //minimum sum of all cerenkov ADCs. 
+    const double min_cerenkov_sum = 1.5e3;  
+    const double min_Esh_Eps_sum  = 0.45;
+
+    const double central_momentum = 1104.;  //in MeV
+
+    //this accounts for the fact that the y-raseter was not calibrated correctly for these runs. 
+    const double y_correction = +1.72835e-3;
+
+    //select the target chosen
+    OpticsTarget_t target; 
+    try { target = ApexOptics::GetTarget(string(target_name)); } 
+
+    catch (const std::exception& e) {
+
+        Error(here, "Something went wrong trying to get the target info.\n what(): %s", e.what()); 
+        return -1; 
+    }
+    
+    printf("Optics target chosen is '%s'.\n", target.name.c_str()); 
+
+    //try to figure out which arm we're using. quit if something goes wrong. 
+    auto is_RHRS_opt = Get_TParameter_from_TFile<bool>(path_infile, "is_RHRS"); 
+    if (!is_RHRS_opt.has_value()) {
+        Error(here, "Something went wrong trying to get the 'is_RHRS' parameter from the file: '%s'", path_infile); 
+        return -1; 
+    }    
+    const bool is_RHRS = is_RHRS_opt.value(); 
+
+    //try to initialize the optics model
+    ChainedOpticsModel* model = new ChainedOpticsModel(is_RHRS); 
+
+    try {
+        model->CreateChainRev({ // sv <= [Poly] q1-fwd <= _Poly_ <= fp 
+            {"data/csv/poly_fits_fp_q1-fwd_L_4ord.dat", branches_fwd_q1, 4}, 
+            {"data/csv/poly_prod_q1_sv_L_4ord.dat",     branches_sv,     5} 
+        }); 
+
+        model->CreateChainFwd({ // sv => [Poly] => fp
+            {"data/csv/poly_prod_sv_fp_L_4ord.dat", branches_fp, 5} 
+        }); 
+    
+    } catch (const std::exception& e) {
+        Error(here, "Something went wrong trying to build the polynomial chains.\n what(): %s", e.what()); 
+        return -1; 
+    }
+
+    //try and make the RDataFrame
+    RDFNodeAccumulator rna(tree_name, path_infile); 
+
+    rna.Define("Xsv", [](double x, double y, double dxdz, double dydz, double dpp)
+        {
+            return Trajectory_t{x, y, dxdz, dydz, dpp}; 
+        }, {"x_sv", "y_sv", "dxdz_sv", "dydz_sv", "dpp_sv"});
+
+    rna.Define("Xfp", [](double x, double y, double dxdz, double dydz)
+        {
+            return Trajectory_t{x, y, dxdz, dydz}; 
+        }, {"x_fp", "y_fp", "dxdz_fp", "dydz_fp"}); 
+    
+    //now, we can compute where each sieve-hole SHOULD be
+    vector<SieveHole> sieve_holes = ApexOptics::ConstructSieveHoles(is_RHRS); 
+
+    auto FindSieveHole = [&sieve_holes](int row, int col) 
+    {
+        auto it = std::find_if( 
+            sieve_holes.begin(),
+            sieve_holes.end(), 
+            [row,col](const SieveHole& elem){ return row==elem.row && col==elem.col; }
+        ); 
+        
+        if (it == sieve_holes.end()) { 
+            return std::optional<SieveHole>(std::nullopt); 
+        } else {
+            return std::optional<SieveHole>(SieveHole{*it});
+        }   
+    }; 
+
+    
+    //get the react vertex
+    //we're going to perfom these calculations to get the (average) dx/dz and dy/dz values for each hole. 
+
+
+    //first, we must compute the react vertex's position in the Sieve Coordinate System (SCS), if it has not already been done. 
+    rna.DefineIfMissing("position_vtx_scs", [is_RHRS](TVector3 vtx_hcs)
+        {
+            return ApexOptics::HCS_to_SCS(is_RHRS, vtx_hcs); 
+        }, {"position_vtx"});
+
+    //now, we compute its average value of the react vertex. 
+    const TVector3 position_vtx_scs(
+        *(rna.Get().Define("val", [](TVector3 vtx){ return vtx.x(); }, {"position_vtx_scs"}).Mean("val")), 
+        *(rna.Get().Define("val", [](TVector3 vtx){ return vtx.y(); }, {"position_vtx_scs"}).Mean("val")), 
+        *(rna.Get().Define("val", [](TVector3 vtx){ return vtx.z(); }, {"position_vtx_scs"}).Mean("val"))
+    );
+    
+    //once we have this, we can compute the average slope to exect for each hole. 
+    const double sieve_dz = 12.5e-3; //in meters
+    const double dx = 5.842e-3; //all units in meters
+    const double dy = 4.826e-3;
+
+    struct HolePosWithAngles_t { 
+        int row, col; 
+        double dxdz_sv, dydz_sv;
+    };
+    std::vector<HolePosWithAngles_t> sieve_hole_angles; 
+    for (const auto hole : sieve_holes) {
+
+        //first, we 'aim' at the front of the sieve 
+        double dxdz_front = (hole.x - position_vtx_scs.x())/(0. - position_vtx_scs.z()); 
+        double dydz_front = (hole.y - position_vtx_scs.y())/(0. - position_vtx_scs.z()); 
+
+        //then, we 'aim' at the back of the sieve hole's exit 
+        double dxdz_back  = (hole.x - position_vtx_scs.x())/(sieve_dz - position_vtx_scs.z()); 
+        double dydz_back  = (hole.y - position_vtx_scs.y())/(sieve_dz - position_vtx_scs.z()); 
+
+        //we take the average of these to to determine the best spot to 'guess' the center of the sieve hole's true position 
+        sieve_hole_angles.push_back({
+            .row = hole.row, 
+            .col = hole.col, 
+            .dxdz_sv = 0.5*(dxdz_front + dxdz_back),
+            .dydz_sv = 0.5*(dydz_front + dydz_back)
+        }); 
+    }
+
+
+
+
+    auto hist_dx_dy_ptr = rna.Get().Histo2D<double>({"h", "hist xy", 100, -0.04,0.03, 200, -0.05,0.05}, "dxdz_sv","dydz_sv"); 
+
+    auto hist_dx_dy = (TH2D*)hist_dx_dy_ptr->Clone("hist_dx_dy"); 
+
+    auto c = new TCanvas; 
+    gStyle->SetOptStat(0); 
+    gStyle->SetPalette(kBird); hist_dx_dy->GetZaxis()->SetNdivisions(200.); 
+    hist_dx_dy->Draw("col"); 
+ 
+
+
+    //range of columns and rows to try to fit 
+    const int row_0 = 3; 
+    const int row_1 = 12; 
+    
+    const int col_0 = 1; 
+    const int col_1 = 10; 
+
+    //statistical constraints (to avoid fitting cells which have no stats)
+    
+    //for each fit subrange, the maximum bin-value in that subrange must be at least this fraction of the 
+    // histogram's global maximum. otherwise, a fit is not attempted. 
+    const double subrange_max_ratio_min = 0.025;    
+
+
+    //fraction of the spacing to the next hole to make our cut. 
+    // '=1.' corresponds to a cut which spans to the center of the next hole on either side. 
+    const double cut_width_row = 1.25; 
+    const double cut_width_col = 0.40;  
+
+    //cache the sieve-coords and fp-coords in memory, to make it faster. 
+    auto df_cache = rna.Get().Cache({"Xsv", "Xfp"}); 
+
+    //loop thru all the sieve holes, and try to fit the ones (which exist). 
+    for (int row=row_0; row<=row_1; row++) {
+        for (int col=col_0; col<=col_1; col++) {
+
+            //find the corresponding sieve hole. if it doesn't exist, then skip it. 
+            auto hole_find = FindSieveHole(row, col);
+            if (!hole_find.has_value()) continue; 
+
+            const auto hole = hole_find.value(); 
+
+            //find the sieve hole angles we computed before
+            HolePosWithAngles_t hole_angle{.row=-1}; 
+            for (const auto& ha : sieve_hole_angles) { 
+                if (ha.row == hole.row && ha.col == hole.col) { hole_angle = ha; break; }
+            }
+            if (hole_angle.row==-1) {
+                Error(here, "Something went wrong trying to find the correct 'HolePosWithAngles_t'. row=%i, col=%i", 
+                    hole.row, hole.col    
+                ); 
+                return -1;
+            }
+
+            const double cut_halfwidth_x = dx*cut_width_row; 
+            const double cut_halfwidth_y = dy*cut_width_col; 
+
+            //get the fit subrange
+            const double 
+                dxdz0{hole_angle.dxdz_sv - cut_halfwidth_x}, 
+                dydz0{hole_angle.dydz_sv - cut_halfwidth_y}, 
+
+                dxdz1{hole_angle.dxdz_sv + cut_halfwidth_x}, 
+                dydz1{hole_angle.dydz_sv + cut_halfwidth_y}; 
+
+
+            double subrange_max = GetMaximum_in_subrange(hist_dx_dy, dxdz0,dydz0, dxdz1,dydz1); 
+            double global_max   = hist_dx_dy->GetMaximum(); 
+            //check the ratio of the subrange max to the global max. if it's too small, then don't attemt a fit. 
+            if (subrange_max / global_max < subrange_max_ratio_min) continue; 
+
+
+            
+            //try to fit a gaussian to the data 
+            auto gauss_fit_result_optional = Gauss2D::Fit_TH2D(
+                hist_dx_dy,
+                dxdz0, dydz0,
+                dxdz1, dydz1
+            ); 
+
+            //if the fit did not succeed, then skip. 
+            if (!gauss_fit_result_optional.has_value()) continue; 
+
+            //make reasonable cuts on what the result should be
+            auto fit = gauss_fit_result_optional.value(); 
+            if (fit.cent_x < dxdz0 || fit.cent_x > dxdz1 ||
+                fit.cent_y < dydz0 || fit.cent_y > dydz1)
+                continue; 
+
+            //now, we have our angles and we're ready to proceed. 
+            auto box = new TBox(
+                dxdz0, dydz0, 
+                dxdz1, dydz1
+            ); 
+            box->SetFillStyle(0);
+            box->SetLineColor(kBlack); 
+            box->SetLineStyle(kDotted); 
+            box->Draw(); 
+            
+            
+            auto tf2 = Gauss2D::Make_TF2_from_fit(gauss_fit_result_optional.value(), "gauss2d"); 
+            tf2->DrawCopy("SAME"); 
+
+        }
+    }
+
+    return 0; 
+}
