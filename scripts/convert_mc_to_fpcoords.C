50414a26fe526d757864bcdea58daa387a45ea99
sethcarl2000 <sethhall@jlab.org>
2025-11-20 14:11:30 -0500

    Bring 'convert_mc_to_fpcoords.C' up-do-date with minor changes

--- scripts/convert_mc_to_fpcoords.C
+++ scripts/convert_mc_to_fpcoords.C
@@ -2,7 +2,9 @@
 #include <ROOT/RDataFrame.hxx>
 #include <TVector3.h> 
 #include <TParameter.h>
+#include <ApexOptics.h> 
 #include "include/Add_TParameter_to_TFile.h" 
+#include "include/Get_TParameter_from_TFile.h"
 
 using namespace std; 
 using namespace ROOT::VecOps; 
@@ -15,6 +17,15 @@ int convert_mc_to_fpcoords(const char* path_infile, const char* path_outfile, co
     ROOT::RDataFrame df(tree_name, path_infile); 
 
     double hrs_momentum = 1104.; 
+
+    //try to get 'is_RHRS' parameter from our input file
+    auto param = Get_TParameter_from_TFile<bool>(path_infile, "is_RHRS");
+    if (!param.has_value()) {
+        Error("convert_mc_to_fpcoords", "Unable to extract 'is_RHRS' parameter from TFile: '%s'", path_infile); 
+        return -1; 
+    }
+    const bool is_RHRS = param.value(); 
+
 
     cout << "Creating output file..." << flush; 
 
@@ -31,6 +42,8 @@ int convert_mc_to_fpcoords(const char* path_infile, const char* path_outfile, co
         .Define("dydz_q1",  [](TVector3 v){ return v.y()/v.z(); }, {"momentum_Q1"})
         .Define("dpp_q1",   [hrs_momentum](TVector3 v){ return (v.Mag()-hrs_momentum)/hrs_momentum; }, {"momentum_Q1"})
         
+        .Define("position_vtx_scs", [is_RHRS](TVector3 vtx){ return ApexOptics::HCS_to_SCS(is_RHRS, vtx); }, {"position_vtx"})
+
         .Snapshot(tree_name, path_outfile, {
                 "x_fp",
                 "y_fp",
@@ -50,6 +63,7 @@ int convert_mc_to_fpcoords(const char* path_infile, const char* path_outfile, co
                 "dpp_sv",
 
                 "position_vtx",
+                "position_vtx_scs",
                 "momentum_vtx"
             });
 
@@ -57,18 +71,8 @@ int convert_mc_to_fpcoords(const char* path_infile, const char* path_outfile, co
 
     cout << "Adding 'is_RHRS' parameter..." << flush; 
 
-    auto file = new TFile(path_infile, "READ"); 
-
-    auto param_is_RHRS = (TParameter<bool>*)file->Get("is_RHRS"); 
-
-    const bool is_RHRS = param_is_RHRS->GetVal(); 
-
-    file->Close(); 
-    delete file; 
-    file = new TFile(path_outfile, "UPDATE"); 
-
-    Add_TParameter_to_TFile("is_RHRS", is_RHRS); 
-            
+    auto file = new TFile(path_outfile, "UPDATE"); 
+    Add_TParameter_to_TFile<bool>("is_RHRS", is_RHRS); 
     file->Close(); 
 
     cout << "done." << endl; 
